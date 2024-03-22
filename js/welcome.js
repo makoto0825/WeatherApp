dd58a0e33be123eb634c994ccb1a58dcdc09d678
myktsk <22167483+myktsk@users.noreply.github.com>
2024-03-22 09:10:26 -0400

    separate js file

---
+++ js/welcome.js
@@ -0,0 +1,41 @@
+// ===========================
+// Switching Sliders
+// ===========================
+function scrollSlide(direction) {
+  const sliderContent = document.getElementById("sliderContent");
+  const currentNumber = parseInt(sliderContent.getAttribute("current"));
+
+  sliderContent.setAttribute(
+    "current",
+    direction === "next" ? currentNumber + 1 : currentNumber - 1
+  );
+}
+
+const nextButtons = document.querySelectorAll(".slide_button_next");
+nextButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    scrollSlide("next");
+  });
+});
+
+const prevButtons = document.querySelectorAll(".slide_button_prev");
+prevButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    scrollSlide("prev");
+  });
+});
+
+// Clean up
+window.addEventListener("unload", () => {
+  nextButtons.forEach((button) => {
+    button.removeEventListener("click", () => {
+      scrollSlide("next");
+    });
+  });
+
+  prevButtons.forEach((button) => {
+    button.removeEventListener("click", () => {
+      scrollSlide("prev");
+    });
+  });
+});
