8647acae41501bafd3c580a1249d8700847b9c9e
sigehiro <sigehiro.0728@gmail.com>
2024-03-25 14:14:08 -0400

    fix: クラス名、フィイル名の修正

--- js/detailed_weather.js
+++ js/detailed_weather.js
@@ -1,6 +1,6 @@
 //Scroll to the top of the page when a specific day of the week element is clicked
 document.addEventListener('DOMContentLoaded', () =>{
-    const rows = document.querySelectorAll('#js-dailyForcast-scroll');
+    const rows = document.querySelectorAll('.js-scrollTop');
   
     rows.forEach(row => {
       row.addEventListener('click', () =>{
